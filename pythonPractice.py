af8cfe4d7c702a7c5d75bd8c98dfb54255bc9b68
jairo <jairopaulino5822@gmail.com>
2025-01-26 12:23:40 -0400

    (learning):Casting in python

--- pythonPractice.py
+++ pythonPractice.py
@@ -4,3 +4,7 @@
 # Another to display a variable
 versionNumber=3.001
 print(f'This the version number{ versionNumber}')
+#casting varibles
+salary_Amount=float(300000.06)
+
+print(f'This is your salary {salary_Amount}')
